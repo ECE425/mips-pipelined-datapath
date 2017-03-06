4db50fc335707d0c2cf1b2885e3fcf66903dd36d
matthew-novoa <matthew.novoa@icloud.com>
2017-03-05 19:51:16 -0800

    Behavioral Register File
    ***Has not been tested. Still needs to be simulated.

--- Register_File.v
+++ Register_File.v
@@ -23,20 +23,35 @@
 module Register_File(A,B,Aaddr, Baddr, Caddr, C,clr,load, clk);
 output [15:0] A;// output port
 output [15:0] B;// output port
-input clr;
+
 input [3:0]Aaddr;// data address A
 input [3:0]Baddr;// data address B
-input [3:0]Caddr;// data address C
-input [15:0] C;// data input port
+input [3:0]Caddr;// data address C for which register to write into 
+
+input [15:0] C;// data input port for data that will be written 
 
 //when asserted, all register values should be set to 0
 input clr;// clear signal
 // when asserted, rising edge on clk causes data input on C to be stored in Caddr
-input load;// load signal 
+input load;// load signal, used for enabling write 
 
 input clk;// rising edge trigger clock
 
+// creates a 16 X 16 Register file 
+//   number of bits X number of registers 
+reg [15:0] register [15:0];
+// Dont think Aaddr==4'b000, i think it should be if clr then clear the data.                                       
+assign A= (Aaddr== 4'b0000) ? 16'b0000_0000_0000_0000 : register[Aaddr];
+assign B= (Baddr== 4'b0000) ? 16'b0000_0000_0000_0000 : register[Baddr];
 
+always @(posedge clk)
+begin 
+
+if(load== 1'b1)
+    begin 
+        register[Caddr]<=C;
+    end
+end
 
 
 endmodule
