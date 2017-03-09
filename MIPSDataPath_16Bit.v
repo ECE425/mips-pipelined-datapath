b6529d70fd725cac99545dd2b7c4c8e4eaea9475
matthew-novoa <matthew.novoa@icloud.com>
2017-03-08 17:52:01 -0800

    Datapath Update 3-8-2017

--- MIPSDataPath_16Bit.v
+++ MIPSDataPath_16Bit.v
@@ -30,13 +30,28 @@ wire [15:0]Instr;// Output from Instruction Memory, carying insstruction
 wire[3:0] Caddr;// address of C ontent
 
 //File Register
-wire [15:0] A,B;// File reg outputs
+wire [15:0] A,B;// File reg outputs (Read Data 1&2)
+wire [15:0] B_ALU_data;// Output from MUX that selects sign extend or B (Read Data 2)going into ALU
+wire [15:0]C;// output from data memory into File_Reg, this is data not an address
 
 //Control Unit
 wire [10:0] control;
 
 //ALU
 wire [15:0]ALuresult;
+wire cin;// IDK if used 
+wire ALUcout;// cout from alu 
+wire ALUlt;// less than flag
+wire ALUeq;// BNE flag
+wire ALUv;// overflow indicator
+
+//signExtend
+wire [15:0]SignExtOffset;
+
+//DataMemory
+wire [15:0]ReadData;// Read the data from data memory 
+
+
 
 //**** ControlUnit****
 //Purpose:
@@ -60,6 +75,7 @@ Full_Adder_2s_16Bits PCIncrement (new_pc_inc,pc_V,pc_cout,pc_instr,16'b0000_0000
 // Purpose: Increment PC by 1 and add OFFSET
 Full_Adder_2s_16Bits PCOffset (new_pc_offset,pc_V_offset,pc_cout_offset,new_pc_inc,offset,1'b0);
 
+
 //*** 2. Load PC Instruction (pc_instr) into Data memory 
 //Purpose: The Program counter will load its address and it would output the instruction to be executed. 
 //input: It will input PC Instruction (pc_instr) which is outputted from the PC (ProgramCounter). NOTE: PC is the 
@@ -74,6 +90,7 @@ Full_Adder_2s_16Bits PCOffset (new_pc_offset,pc_V_offset,pc_cout_offset,new_pc_i
 // output [15:0]Instr input [15:0]pc_instr
 Instr_Mem InstructionMemory(Instr,pc_instr);
 
+
 //*** 3. RegDst Mux2x1_4bit
 //Purpose: Destination of address for register to recieve to have the address where contenet will be written too 
 //input: Rt=Instr[7:4] Rd=Instr[3:0]
@@ -83,7 +100,8 @@ Instr_Mem InstructionMemory(Instr,pc_instr);
 //input s"select" a(4bits) b(4bits) E"enable" , output out(4bit)"Caddr"
 Mux2X1_4Bit RegDst_Mux(control[0],Instr[7:4],Instr[3:0],1'b1,Caddr);
 
-//*** 4. Register File
+
+//*** 4. Register File and Sign Extend
 //Purpose:
 //input:
 //output:
@@ -93,13 +111,45 @@ Mux2X1_4Bit RegDst_Mux(control[0],Instr[7:4],Instr[3:0],1'b1,Caddr);
 //control[9]=load=RegWrite in datapath 
 Register_File RegFile(A,B,Instr[11:8], Instr[7:4], Caddr, "C","clr",control[9], clk);
 
+//Sign Extend 
+//Purpose:
+//input: 
+//output:
+//module SignExt_4to16(Dout16,Din4);
+SignExt_4to16 SignExtend(SignExtOffset,Instr[3:0]);
+
+
+//*** 5. ALU Source and Execution 
+// AluSrc Selection
+//Purpose:Select between Read Data 2 (B) from Reg_File or SignExtOffset (16bits)
+//input: 
+//output:
+//module Mux2X1_16Bit (s,a,b,E,out);
+Mux2X1_16Bit ALUsrc_Mux(control[8],B,SignExtOffset,1'b1,B_ALU_data);
 
 //***ALU
+//Purpose:Select between Read Data 2 (B) from Reg_File or SignExtOffset (16bits)
+//input: 
+//output:
 //module ALU(X,Y,out,Cin,Cout,lt,eq,gt,V,opcod);
-ALU MIPSalu(A,B,ALUresult,"cin","cout","lt","eq","gt","V",control[6:4]);
+ALU MIPSalu(A,B_ALU_data,ALUresult,"cin",ALUcout,ALUlt,ALUeq,"gt",ALUv,control[6:4]);
 //***ALUControl
 
-//***SignExtend
+
+//***6. Data Memory and Memory to Register  
+//Purpose:
+//input: 
+//output:
+//module DataMemory(ReadData,ALUresultAddr,WriteData,MemWrite,MemRead,clk);
+DataMemory DataMem(ReadData,ALUresult,B,control[7],control[2],clk);
+
+//MemtoReg
+//Purpose:
+//input: 
+//output:Output is C (data) it is passed back into the Reg_File to write the content into Reg_File on Caddr
+//module Mux2X1_16Bit (s,a,b,E,out);
+Mux2X1_16Bit MemtoReg_Mux(control[3],ALUresult,ReadData,1'b1,C);
+
 
 //***Branch
 endmodule
